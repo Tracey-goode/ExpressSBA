49ef5b68c4301ffe2da03121395945f043532362
Tracey-goode <Jerand322@gmail.com>
2025-09-14 19:39:52 -0400

    boilerplate

--- app.mjs
+++ app.mjs
@@ -5,3 +5,18 @@ import logger from './middleware/logger.mjs';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+//Middleware
+app.use(logger);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//static files
+app.use(express.static('public'));
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+//routes
+app.get('/', (req, res) => {
+    res.render('index');
+});
